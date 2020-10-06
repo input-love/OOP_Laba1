e08d65ecb4854a3af1f9cfd5cd5aa980e96e6f86
Andrew <input_love@users.noreply.github.com>
2020-10-06 21:27:06 +0300

    comments

--- OOP_Laba1/OOP_Laba1/Queue.cpp
+++ OOP_Laba1/OOP_Laba1/Queue.cpp
@@ -6,45 +6,44 @@ Queue::Queue() = default;
 Queue::Queue(const int& size): _sizeArr(size), _arr(new int[_sizeArr]) {}
 
 Queue::~Queue() {
-    delete[] _arr;
+    delete[] _arr; // Очистка памяти
 }
 
 void Queue::push(const int& data) {
-    if (_count != _sizeArr) {
-        if (_tail == _sizeArr - 1) {
+    if (_count != _sizeArr) { // Чтобы новый элемент не выходил за размеры очереди
+        if (_tail == _sizeArr - 1) { // Когда Хвост в конце очереди 
             _tail = -1;
         }
-        *(_arr + (++_tail)) = data;
-        _count++;
+        *(_arr + (++_tail)) = data; // Добавление элемента в Хвост
+        _count++; // Кол-во элементов в очереди
     } else {
-        throw Error("Очередь переполнена");
+        throw Error("Очередь переполнена"); // Ошибкинс
     }
 }
 
 void Queue::pop() {
-    if (_count > 0) {
-        _head++;
-        if (_head == _sizeArr) {
+    if (_count > 0) { // Существует ли элемент в очереди 
+        _head++; // Сдвиг Головы  ( типо удаление )
+        if (_head == _sizeArr) { // Когда Голова в конце массива 
             _head = 0;
         }
-        _count--;
+        _count--; // Уменьшение кол-ва элементов в очереди
     } else {
-        throw Error("Нельзя удалить элемент - очередь пустая");
+        throw Error("Нельзя удалить элемент - очередь пустая"); // Ошибкинс
     }
 }
 
 bool Queue::isEmpty() const {
-    if (_count > 0) {
-        return !_count;
+    if (_count > 0) { // Существует ли элемент в очереди 
     } else {
-        throw Error("Очередь пустая");
+        throw Error("Очередь пустая"); // Ошибкинс
     }
 }
 
 int Queue::front() const {
-    if (_count > 0) {
-        return *(_arr + _head);
+    if (_count > 0) { // Существует ли элемент в очереди 
+        return *(_arr + _head); // Возращение Головы ( сам элемент )
     } else {
-        throw Error("В очереди нет элементов");
+        throw Error("В очереди нет элементов"); // Ошибкинс
     }
 }
